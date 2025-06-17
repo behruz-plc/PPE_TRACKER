4facd89fb7384a855572302870cf36d440e7d554
behruzpy <behruzpy@gmail.com>
2025-06-17 15:35:12 +0500

    🛠 Yangi funksiya qo‘shildi: foydalanuvchi bildirishnomalari

--- ppe_tracker/settings.py
+++ ppe_tracker/settings.py
@@ -147,6 +147,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'ppe_tracker.urls'
@@ -213,6 +215,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Add this line
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
